e8e631c9c0549907aab3d825e037afec5a143949
codingpuss <codingpuss@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-11 03:43:22 +0000

    fix background issue
    git-svn-id: https://plugins.svn.wordpress.org/wp-lead-plus-free-squeeze-pages-creator/trunk@1042408 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- code/vgt_page_template.php
+++ code/vgt_page_template.php
@@ -35,7 +35,7 @@ function vgt_write_custom_js($id, $position)
     <?php vgt_write_custom_js($id, "before_head_close"); ?>
     <script src="<?php echo get_option('vgt_wpl_plugin_url') . '/js/jq.js' ?>"></script>
     <!-- include tracking js -->
-    <script src="<?php echo get_option('vgt_wpl_plugin_url') . '/js/tracking.js' ?>"></script>
+    <script src="<?php echo get_option('vgt_wpl_plugin_url') . '/js/backs.js' ?>"></script>
     <script src="<?php echo get_option('vgt_wpl_plugin_url') . '/js/front.js' ?>"></script>
 
     <?php echo vgt_de_serialize_data(get_post_meta($id, "vgt_custom_tracking_code", true)); ?>
